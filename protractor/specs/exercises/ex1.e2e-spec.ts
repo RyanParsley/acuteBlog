2e7616fe949136f5a5bbe85db35e117d3882ab62
Ryan Parsley <rmparsley@gmail.com>
2019-10-25 09:13:56 -0400

    typo

--- protractor/specs/exercises/ex1.e2e-spec.ts
+++ protractor/specs/exercises/ex1.e2e-spec.ts
@@ -1,7 +1,7 @@
 import { browser, element, by } from 'protractor';
 
 describe('AcuteBlog App: Exercise 1', () => {
-  it('should be a thing that does not impload', () => {
+  it('should be a thing that does not implode', () => {
     // Visit the app
 
     // Make an assertion that the title of the landing page is 'Acute Blog'
